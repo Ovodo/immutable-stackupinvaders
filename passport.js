5369d447f45bf6af9cf416f0432e9faa500546f1
Ovodo <ovdizzle@gmail.com>
2023-12-17 17:40:33 +0100

    The Invaders initialized

--- passport.js
+++ passport.js
@@ -1,11 +1,10 @@
 window.passport = new window.immutable.passport.Passport({
-    baseConfig: new window.immutable.config.ImmutableConfiguration({
-      environment: window.immutable.config.Environment.SANDBOX,
-    }),
-    clientId: 'enter-client-id',
-    redirectUri: 'redirect-url',
-    logoutRedirectUri: 'logout-url',
-    audience: 'platform_api',
-    scope: 'openid offline_access email transact'
-  });
-
+  baseConfig: new window.immutable.config.ImmutableConfiguration({
+    environment: window.immutable.config.Environment.SANDBOX,
+  }),
+  clientId: "vuNkF06sKRhzhdH5UptcPWXVT07gGVVl",
+  redirectUri: "https://kid-proper-falcon.ngrok-free.app",
+  logoutRedirectUri: "https://kid-proper-falcon.ngrok-free.app/logout.html",
+  audience: "platform_api",
+  scope: "openid offline_access email transact",
+});
